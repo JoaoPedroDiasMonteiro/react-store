c367c14c4727e6fbef242906cc35cf53e999c297
João Pedro Dias <joao.dias@devsquad.com>
2024-04-01 15:23:14 -0300

    Add header and footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import ProductList from "./components/Product/ProductList.tsx"
 import { Outlet } from "react-router-dom"
+import Header from "./components/header/header.tsx"
+import Footer from "./components/footer/footer.tsx"
 
 export default function App() {
 
   return <>
-    <ProductList />
+    <Header />
+    {/* <ProductList /> */}
     <Outlet />
+    <Footer />
   </>
 }
