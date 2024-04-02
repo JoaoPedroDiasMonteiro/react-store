b3784292b5d610662664bcf0a3ef9d094d032c56
João Pedro Dias <joao.dias@devsquad.com>
2024-04-02 08:12:16 -0300

    WIP

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -18,7 +18,7 @@ export default function Register() {
                             alt="Your Company"
                         />
                         <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                            Register your account
+                            Sign up your account
                         </h2>
                         <p className="mt-2 text-sm leading-6 text-gray-500">
                             Already a member?{' '}
@@ -60,7 +60,7 @@ export default function Register() {
                                         type="submit"
                                         className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     >
-                                        Create account
+                                        Sign up
                                     </button>
                                 </div>
                             </form>
