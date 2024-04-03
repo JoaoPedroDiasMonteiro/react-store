818cb7c7b609541fd69e6abeef991b3045a0c596
João Pedro Dias <joao.dias@devsquad.com>
2024-04-03 18:41:25 -0300

    Adjust alignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
-import Header from "./components/header/header.tsx"
 import Footer from "./components/footer/footer.tsx"
+import Header from "./components/header/header.tsx"
 
 export default function App() {
-
-  return <>
-    <Header />
-    <Outlet />
-    <Footer />
-  </>
+    return <>
+        <Header />
+        <Outlet />
+        <Footer />
+    </>
 }
